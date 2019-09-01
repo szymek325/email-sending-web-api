07762ad45e5acdaa6cc50a35e17f30ab531c0934
matszym325 <matszym325@gmail.com>
2019-09-01 11:46:04 +0200

    get and getbyId in controller

--- src/EmailSending.Web.Api/Controllers/EmailController.cs
+++ src/EmailSending.Web.Api/Controllers/EmailController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using EmailSending.Web.Api.CustomExceptions;
+using EmailSending.Web.Api.DataAccess;
 using EmailSending.Web.Api.DataAccess.Entities;
 using EmailSending.Web.Api.Dto;
 using EmailSending.Web.Api.Services;
@@ -14,13 +15,16 @@ namespace EmailSending.Web.Api.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class EmailController : ControllerBase
     {
+        private readonly IEmailsRepository _emailsRepository;
         private readonly ILogger<EmailController> _logger;
         private readonly IRequestOrchestrator _requestOrchestrator;
 
-        public EmailController(IRequestOrchestrator requestOrchestrator, ILogger<EmailController> logger)
+        public EmailController(IEmailsRepository emailsRepository, ILogger<EmailController> logger,
+            IRequestOrchestrator requestOrchestrator)
         {
-            _requestOrchestrator = requestOrchestrator;
+            _emailsRepository = emailsRepository;
             _logger = logger;
+            _requestOrchestrator = requestOrchestrator;
         }
 
         [HttpPost]
@@ -43,6 +47,20 @@ namespace EmailSending.Web.Api.Controllers
                 _logger.LogError(ex, "error occured");
                 return new BadRequestObjectResult(new {guid, ex.Message});
             }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var emails = await _emailsRepository.GetAll();
+            return new OkObjectResult(emails);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            var emails = await _emailsRepository.Get(id.ToString());
+            return new OkObjectResult(emails);
         }
     }
 }
